f8a0ded9fbba4caeae65ee2bb3c48f8d4cf249c1
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-13 18:02:05 +0300

    feat: added some styling to accordion component

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -9,18 +9,18 @@ export default function Accordion({ items }) {
       {items.map((item, index) => {
         const isExpanded = index === expandedIndex
         const icon = (
-          <span>
+          <span className=''>
             {isExpanded ? <BiChevronDown /> : <BiChevronRight />}
           </span>
         )
 
         return (
-          <div key={item.id}>
-            <div onClick={() => setExpandedIndex(index)}>
-              {icon}
+          <div className='border-x border-t rounded' key={item.id}>
+            <div className='flex p-3 bg-gray-50 border-b items-center cursor-pointer justify-between' onClick={() => setExpandedIndex(index)}>
               {item.label}
+              {icon}
             </div>
-            {isExpanded &&  <div>{item.content}</div>}
+            {isExpanded &&  <div className='border-b p-5'>{item.content}</div>}
           </div>
         )
       })}
