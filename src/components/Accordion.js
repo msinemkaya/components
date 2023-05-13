ed1aefed7f116ea580181f922da02812b207375e
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-13 22:23:12 +0300

    feat: added a countermasure for a small bug that might happen

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -5,11 +5,28 @@ export default function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(-1)
 
   const handleClick = (index) => {
-    if(expandedIndex === index) {
-      setExpandedIndex(-1)
-    }else{
-      setExpandedIndex(index)
-    }
+    // if(expandedIndex === index) {
+    //   setExpandedIndex(-1)
+    // }else{
+    //   setExpandedIndex(index)
+    // }
+
+    // the reason we are doing this instead is because there is a small bug 
+    // in react that if you update a state react takes its time a bit incase you xhange multiple states at once
+    // to executethem at the same time to make the app faster. 
+    // but because of this if you trigger the same event fast enough, state will be at its old value because of this delay
+    // and you wont get the most recent value
+    // you dont need to think about it if your state is independent from its old value
+    // but if the value is dependent on the old one, then its unlikely because of the speed but still you can consider this,
+    // to fix that we are calling a function inside of the setter function so it can take the most up to date value
+    setExpandedIndex((currentExpandedIndex) => {
+      if(currentExpandedIndex === index) {
+        return -1;
+      }
+      else {
+        return index;
+      }
+    })
   }
 
   return (
