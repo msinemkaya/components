08e0bcf6c1a8b2b07bc4f973d252f4016525bb8d
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-13 17:38:40 +0300

    feat: added icons to accordions

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { BiChevronDown, BiChevronRight } from 'react-icons/bi'
 
 export default function Accordion({ items }) {
   const [expandedIndex, setExpandedIndex] = useState(0)
@@ -7,10 +8,18 @@ export default function Accordion({ items }) {
     <>
       {items.map((item, index) => {
         const isExpanded = index === expandedIndex
+        const icon = (
+          <span>
+            {isExpanded ? <BiChevronDown /> : <BiChevronRight />}
+          </span>
+        )
 
         return (
           <div key={item.id}>
-            <div onClick={() => setExpandedIndex(index)}>{item.label}</div>
+            <div onClick={() => setExpandedIndex(index)}>
+              {icon}
+              {item.label}
+            </div>
             {isExpanded &&  <div>{item.content}</div>}
           </div>
         )
