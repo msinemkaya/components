51a9bb75c158fe975dc2aa7226649006efb3e88d
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-16 01:46:55 +0300

    feat: some small bug precautions

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,16 @@
+import { useEffect } from 'react';
 import ReactDOM from 'react-dom'
 
 export default function Modal({ onClose, children, actionBar }){
+
+  // to prevent user from scrolling when modal pops up
+  useEffect(() => {
+    document.body.classList.add('overflow-hidden')
+
+    return () => {
+      document.body.classList.remove('overflow-hidden')
+    }
+  }, [])
 
   // with portal you are telling react to place the html produced by this component to the given place in document
   // insted of where it would be normally
@@ -13,8 +23,8 @@ export default function Modal({ onClose, children, actionBar }){
   // thats why insted of putting it deep inside we are creating a portal on the html document to place it where we want
   return ReactDOM.createPortal(
     <div>
-      <div onClick={onClose} className='absolute inset-0 bg-gray-300 opacity-80'></div>
-      <div className='absolute inset-40 p-10 bg-white'>
+      <div onClick={onClose} className='fixed inset-0 bg-gray-300 opacity-80'></div>
+      <div className='fixed inset-40 p-10 bg-white'>
         <div className='flex flex-col h-full justify-between'>
           {children}
           <div className='flex justify-end'>
