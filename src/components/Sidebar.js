4d10e0ad6aa66dfab7722f0dde806a942734259c
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-17 00:13:48 +0300

    feat: created a Table Component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,6 +7,7 @@ export default function Sidebar(){
     { label: 'Button', path: '/button'},
     { label: 'Accordion', path: '/accordion'},
     { label: 'Modal', path: '/modal'},
+    { label: 'Table', path: '/table'},
   ]
 
   return(
---
+++ src/components/Table.js
@@ -0,0 +1,24 @@
+export default function Table({ data }){
+  return(
+    <table className='table-auto border-spacing-2'>
+      <thead>
+        <tr className='border-b-2'>
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((fruit) => (
+          <tr className='border-b' key={fruit.name}>
+            <td className='p-3'>{fruit.name}</td>
+            <td className='p-3'>
+              <div className={`${fruit.color} p-3 m-2`}></div>
+            </td>
+            <td className='p-3'>{fruit.score}</td>
+          </tr> 
+        ))}
+      </tbody>
+    </table>
+  );
+}
