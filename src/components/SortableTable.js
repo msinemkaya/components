42bdfebb114d5de6b365f2ad50003b03b2310ee7
Melike Sinem Kaya <msinemkaya6@gmail.com>
2023-05-17 22:37:43 +0300

    feat: added arrows

--- src/components/SortableTable.js
+++ src/components/SortableTable.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Table from './Table';
+import { GoArrowSmallDown, GoArrowSmallUp } from 'react-icons/go'
 
 export default function SortableTable(props){
 
@@ -16,61 +17,102 @@ export default function SortableTable(props){
     // with adding additional header function to it too
     return {
       ...column,
-      header: () => <th onClick={() => handleClick(column.label)}>{column.label}</th>
+      header: () => (
+        <th className='cursor-pointer hover:bg-gray-100' onClick={() => handleClick(column.label)}>
+          <div className='flex items-center'>
+            {getIcons(column.label, sortBy, sortOrder)}
+            {column.label}
+          </div>
+        </th>
+      )
     }
   })
 
   const handleClick = (label) => {
     // if (sortOrder === null) {
-    //   setSortOrder('asc')
-    // } else if (sortOrder === 'asc') {
-    //   setSortOrder('desc')
-    // } else if (sortOrder === 'desc') {
-    //   setSortOrder(null)
-    // }
-
-    switch (sortOrder) {
-      case null:
-        setSortOrder('asc')
-        setSortBy(label)
-        break;
-      case 'asc':
-        setSortOrder('desc')
-        setSortBy(label)
-        break;
+      //   setSortOrder('asc')
+      // } else if (sortOrder === 'asc') {
+        //   setSortOrder('desc')
+        // } else if (sortOrder === 'desc') {
+          //   setSortOrder(null)
+          // }
+          
+          switch (sortOrder) {
+            case null:
+              setSortOrder('asc')
+              setSortBy(label)
+              break;
+              case 'asc':
+                setSortOrder('desc')
+                setSortBy(label)
+                break;
       case 'desc':
         setSortOrder(null)
         setSortBy(null)
         break;
-    }
-  }
-  
-  let sortedData = data
-  if(sortOrder && sortBy) {
-    const { sortValue } = config.find(column => column.label === sortBy)
-    
-    // we make a copy of the data because we dont want to change(modify) it directly since it is a prop
-    // we never ever modify a prop directly
-    sortedData = [...data].sort((a,b) => {
-      const valueA = sortValue(a)
-      const valueB = sortValue(b)
-
-      const reverseOrder = sortOrder === 'asc' ? 1 : -1
-
-      if(typeof valueA === 'string') {
-        return valueA.localeCompare(valueB) * reverseOrder
-      }else {
-        return ( valueA - valueB ) * reverseOrder
       }
-
-    })
-  }
-
-  return(
+    }
+    
+    let sortedData = data
+    if(sortOrder && sortBy) {
+      const { sortValue } = config.find(column => column.label === sortBy)
+      
+      // we make a copy of the data because we dont want to change(modify) it directly since it is a prop
+      // we never ever modify a prop directly
+      sortedData = [...data].sort((a,b) => {
+        const valueA = sortValue(a)
+        const valueB = sortValue(b)
+        
+        const reverseOrder = sortOrder === 'asc' ? 1 : -1
+        
+        if(typeof valueA === 'string') {
+          return valueA.localeCompare(valueB) * reverseOrder
+        }else {
+          return ( valueA - valueB ) * reverseOrder
+        }
+        
+      })
+    }
+    
+    return(
     <>
       {/* we pass all the props to table directly */}
       {/* and we kinda overwrite the config prop and data too */}
       <Table {...props} config={updatedConfig} data={sortedData} />
     </>
   );
+}
+
+function getIcons(label, sortBy, sortOrder) {
+  if(label !== sortBy) {
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    )
+  }
+
+  if (sortOrder === null) {
+    return (
+      <div>
+        <GoArrowSmallUp />
+        <GoArrowSmallDown />
+      </div>
+    )
+  }else if (sortOrder === 'asc') {
+    return (
+      <div>
+        <GoArrowSmallUp />
+      </div>
+    )
+  } else if (sortOrder === 'desc') {
+    return (
+      <div>
+        <GoArrowSmallDown />
+      </div>
+    )
+  }
+
+
 }
